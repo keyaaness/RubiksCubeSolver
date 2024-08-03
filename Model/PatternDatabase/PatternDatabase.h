9f44b146ac043e741d6e334c40029c1cf8c621c1
Keya <keyabattu561@gmail.com>
2024-08-03 17:25:54 +0530

    commit

--- Model/PatternDatabase/PatternDatabase.h
+++ Model/PatternDatabase/PatternDatabase.h
@@ -4,5 +4,14 @@
 
 #ifndef PATTERNDATABASE_H
 #define PATTERNDATABASE_H
+template<typename T>
+class PatternDatabaseEstimate {
+private:
 
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
 #endif //PATTERNDATABASE_H
